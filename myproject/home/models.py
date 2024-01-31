af4f4e5c313754ed6e1926676a41b9a1737e4d5b
MinhLong2410-02 <minhlong2002@gmail.com>
2024-01-31 18:47:47 +0700

    update models

--- myproject/home/models.py
+++ myproject/home/models.py
@@ -1,4 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
+    username = None
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    def __str__(self):
+        return self.email + " " + self.username
+
 
 class People(models.Model):
     people_id = models.AutoField(primary_key=True)
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
-
+MEDIA_URL = '/media/'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
@@ -124,6 +124,7 @@ USE_TZ = True
 # Cấu hình cho các tệp tĩnh
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+AUTH_USER_MODEL = 'home.CustomUser'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
