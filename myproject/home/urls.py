5ecc79adde68d9a9551d94a2b652dd0452735a78
MinhLong2410-02 <minhlong2002@gmail.com>
2024-02-01 00:19:47 +0700

    delete

--- myproject/home/urls.py
+++ myproject/home/urls.py
@@ -1,9 +1,10 @@
 # home/urls.py
 
 from django.urls import path
-from .views import index, import_info
+from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
     path('import-info/', import_info, name='import-info'),
+    path('get-people-with-relationships/', get_people_with_relationships, name='get-people-with-relationships')
 ]
--- myproject/home/views.py
+++ myproject/home/views.py
@@ -9,44 +9,48 @@ def index(request):
     
     return render(request, 'home/index.html')  
 
-class StudentView(LoginRequiredMixin, ListView):
-    template_name = 'home/index.html'
-    # model = Student
-    # context_object_name = 'student'
+def get_people_with_relationships(request):
+    # print out the get request with request.GET
+    print(request.GET)
+    return render(request, 'home/index.html')
+# class StudentView(LoginRequiredMixin, ListView):
+#     template_name = 'home/index.html'
+#     # model = Student
+#     # context_object_name = 'student'
     
-    def get_queryset(self):
-        teacher = self.request.user
-        return teacher
+#     def get_queryset(self):
+#         teacher = self.request.user
+#         return teacher
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['teacher'] = self.request.user
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['teacher'] = self.request.user
         
-        context['classes'] = University_class.objects.filter(teacher=self.request.user)
-        context['cached_class_name'] = cache.get('class_name')
-        context['current_link'] = 'home'
+#         context['classes'] = University_class.objects.filter(teacher=self.request.user)
+#         context['cached_class_name'] = cache.get('class_name')
+#         context['current_link'] = 'home'
         
-        student_id = self.kwargs['student_id']
-        # context['student'] = Student.objects.get(student_id=student_id)
-        # context['class'] = University_class.objects.get(class_name = context['student'].class_name)     
+#         student_id = self.kwargs['student_id']
+#         # context['student'] = Student.objects.get(student_id=student_id)
+#         # context['class'] = University_class.objects.get(class_name = context['student'].class_name)     
         
-        # # check if this class is possed by this teacher
-        # if context['class'].teacher != self.request.user:
-        #     raise Http404
+#         # # check if this class is possed by this teacher
+#         # if context['class'].teacher != self.request.user:
+#         #     raise Http404
         
-        # student_list = list(Student.objects.filter(class_name=context['class']).order_by('-score_10').values())
-        # for i in range(len(student_list)):
-        #     student_list[i]['ranking'] = i + 1
-        # context['student_list'] = student_list
+#         # student_list = list(Student.objects.filter(class_name=context['class']).order_by('-score_10').values())
+#         # for i in range(len(student_list)):
+#         #     student_list[i]['ranking'] = i + 1
+#         # context['student_list'] = student_list
         
-        # subject = Subject_student.objects.filter(student_id=student_id)\
-        #     .values("subject__subject_name", "subject__credit", "score_10")
-        # context['subject_list'] = list(subject)
+#         # subject = Subject_student.objects.filter(student_id=student_id)\
+#         #     .values("subject__subject_name", "subject__credit", "score_10")
+#         # context['subject_list'] = list(subject)
         
-        context['notes'] = Note_student.objects.filter(student_id=student_id)
-        context['current_date'] = datetime.now().strftime("%d/%m/%Y")
-        context['iframeUrl'] = get_iframe_url(3, student_id=student_id)
-        return context 
+#         context['notes'] = Note_student.objects.filter(student_id=student_id)
+#         context['current_date'] = datetime.now().strftime("%d/%m/%Y")
+#         context['iframeUrl'] = get_iframe_url(3, student_id=student_id)
+#         return context 
 
 def import_info(request):
     if request.method == 'POST':
