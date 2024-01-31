4498f3fa4669b90db2dea21919398230a496c13e
BapPine <baphongpine@gmail.com>
2024-01-31 23:52:50 +0700

    home banner

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -71,18 +71,18 @@ MEDIA_URL = '/media/'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'lehungba',
-        'USER': 'lehungba',
-        'PASSWORD': 'Lamgiau@123!',
-        'HOST': '45.117.79.16',  # Hoặc địa chỉ IP của máy chủ cơ sở dữ liệu
-        'PORT': '5432',       # Cổng mặc định của PostgreSQL
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'lehungba',
+    #     'USER': 'lehungba',
+    #     'PASSWORD': 'Lamgiau@123!',
+    #     'HOST': '45.117.79.16',  # Hoặc địa chỉ IP của máy chủ cơ sở dữ liệu
+    #     'PORT': '5432',       # Cổng mặc định của PostgreSQL
+    # }
 }
 
 # Password validation
