9d835d330ce5636b4f03a52f41ade60ee0e044e5
MinhLong2410-02 <minhlong2002@gmail.com>
2024-02-01 18:34:58 +0700

    login page

--- myproject/home/forms.py
+++ myproject/home/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
 from .models import People, CustomUser
 
 class PeopleForm(forms.ModelForm):
@@ -20,3 +20,27 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ("email",)
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(label='Gmail',
+        widget=forms.TextInput(attrs={
+            'class': 'input',
+            'type': 'text',
+            'placeholder': 'Nhập gmail',
+            'autofocus': True,
+            'required': 'required'
+        })
+    )
+    
+    password = forms.CharField(label='Password', 
+        widget=forms.PasswordInput(attrs={
+            'class': 'input password',
+            'name': 'pass',
+            'placeholder': 'Mật khẩu',
+            'required': 'required'  
+        }),
+    )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = False
+        self.fields['password'].label = False
--- myproject/home/urls.py
+++ myproject/home/urls.py
@@ -4,6 +4,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('login/', Login.as_view(), name='login'),
+    # path('', HomeView.as_view(), name='home'),
+    
     path('', index, name='index'),
     path('import-info/', import_info, name='import_info'),
     path('find-people/', find_people, name='find_people'),
