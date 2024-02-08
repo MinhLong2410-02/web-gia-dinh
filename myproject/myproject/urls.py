5482dc760a5068d17d652270d51498d71aa96812
MinhLong2410-02 <minhlong2002@gmail.com>
2024-02-08 12:46:49 +0700

    deploy

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -6,6 +6,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('home.urls')),  # Đảm bảo dòng này tồn tại
-    path('', include('home.urls')),
     # Các URL patterns khác
 ]
